52c4bc685dd10fc7f0d5666ba837efcc42dba3c8
sainathd07 <saisainath777@gmail.com>
2024-02-26 17:22:58 -0500

    made changes to config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://sainathdushatti.com/", // replace this with your deployed domain
+  author: "Sainath Dushatti",
+  desc: "I'm Sainath, a developer and student from Youngstown, OH.",
+  title: "Sainath DUshatti",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -26,7 +26,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/sainathd07",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
@@ -34,17 +34,17 @@ export const SOCIALS: SocialObjects = [
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://instagram.com/iamsainathd",
     linkTitle: `${SITE.title} on Instagram`,
     active: true,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/in/sainath-dushatti/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
@@ -56,9 +56,9 @@ export const SOCIALS: SocialObjects = [
   },
   {
     name: "Twitter",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://twitter.com/iamsainathD",
     linkTitle: `${SITE.title} on Twitter`,
-    active: false,
+    active: true,
   },
   {
     name: "Twitch",
